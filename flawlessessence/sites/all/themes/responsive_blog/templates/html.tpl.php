3e5846efb2a7ca9ade2fd0b141b263680f9d0081
Eche Michael <css.nigeria@gmail.com>
2018-05-01 21:11:43 +0100

    first commit

---
+++ flawlessessence/sites/all/themes/responsive_blog/templates/html.tpl.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+<head>
+<?php print $head; ?>
+<title><?php print $head_title; ?></title>
+<?php print $styles; ?>
+<?php print $scripts; ?>
+<!--[if lt IE 9]><script src="<?php print base_path() . drupal_get_path('theme', 'responsive_blog') . '/js/html5.js'; ?>"></script><![endif]-->
+</head>
+<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+ <script type='text/javascript'>(function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://widget.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({ c: 'c6795d36-2620-4572-b4d1-95f7dec7f787', f: true }); done = true; } }; })();</script>
+
+
+
+</body>
+</html>
